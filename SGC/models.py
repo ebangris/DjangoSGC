63c6275df746abf88a7fe714154c6515582a50d5
ebangris <esteban_gris@utp.edu.co>
2020-10-23 23:58:58 -0500

    Se mejoran los indicadores, se agregan encargados y detalles a las tareas

--- SGC/models.py
+++ SGC/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 from datetime import date
 
 class Proceso(models.Model):
@@ -33,13 +34,17 @@ class Procedimiento(models.Model):
 class Tarea(models.Model):
 	nombre = models.CharField(max_length=80)
 	detalles = models.TextField()
-
+	encargado = models.ManyToManyField(User) 
 	creacion = models.DateField(auto_now_add=True)
 	edicion = models.DateField(auto_now=True)
 	
 	#Una tarea pertenece a un procedimiento
 	procedimiento = models.ForeignKey(Procedimiento, on_delete=models.CASCADE)
 
+	def encargados(self):
+		encargados = self.encargado.all()
+		return encargados
+		
 	def __str__(self):
 		return self.nombre
 
@@ -51,6 +56,11 @@ class Indicador(models.Model):
 	
 	actual = models.IntegerField()
 	esperado = models.IntegerField()
-	
+
+	def calc(self):
+		cumplido = (self.actual / self.esperado)*100
+		return int(cumplido)
+
 	def __str__(self):
 		return self.nombre
+	
